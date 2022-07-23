92425dd6dbf297facc712fa9ee12f59216a4a73f
Uemerson Santos <uemerson161@hotmail.com>
2022-07-23 16:19:08 -0300

    DepartmentFormController

---
+++ src/main/java/com/example/application/departmentFormController.java
@@ -0,0 +1,50 @@
+package com.example.application;
+
+import com.example.application.com.exemple.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class departmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void onBtSaveAction() {
+        System.out.println("onBtSaveAction");
+    }
+
+    @FXML
+    public void onBrCancelAction() {
+        System.out.println("onBtCancelAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+
+    }
+}
