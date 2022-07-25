64f974bcb0687e7203b8291d7e5a24ff9acf79f9
Uemerson Santos <uemerson161@hotmail.com>
2022-07-25 20:49:02 -0300

    Saving a nem Department

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.example.workshopjavafxjdbc {
 
     opens com.example.application to javafx.fxml;
     exports com.example.application;
+    exports com.example.model.entities;
+    exports com.example.model.services;
     opens com.example.model.entities;
     opens com.example.model.services;
 }
